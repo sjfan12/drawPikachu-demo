15966c919e0cc779364f11a1d452b27023b86238
sjfan <1029563017@qq.com>
2020-06-19 14:30:11 +0800

    加调速

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
 !function () {
+    $('.actions').on('click', 'button', function (e) {
+        let $button = $(e.currentTarget)
+        let speed = $button.attr('data-speed') //获取button元素
+       // console.log(speed)
+        $button.addClass('active').siblings('.active').removeClass('active')
+        switch (speed) {
+            case 'slow':
+                duration=100
+                break
+            case 'normal':
+                duration=50
+                break
+            case 'fast':
+                duration=10
+                break
+        }
+
+    })
+    //所有的setInterval都可以用setTimeout来替代
+    var duration=50;
     function writeCode(prefix, code, fn) {
         let domCode = document.querySelector('#code')
         let styleTag = document.querySelector('#styleTag')
         let n=0;
 
-        let id = setInterval(()=>{
+        let id = setTimeout(function run(){
             n+=1;
             domCode.innerHTML = prefix + code.substring(0,n)
             styleTag.innerHTML = prefix + code.substring(0,n)
             domCode.scrollTop = domCode.scrollHeight
-            if (n >= code.length){
-                window.clearInterval(id)
+            if (n< code.length){
+                id = setTimeout(run, duration)
+            }else{
                 fn.call()
             }
-        },10)
+        },duration)
     }
 
     var result= `/*我们来画一只皮卡丘吧*/
